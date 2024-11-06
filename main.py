f934b419163c457e0235c5924acc1e22701401c5
korapuzz <33680669+korapuzz@users.noreply.github.com>
2024-11-06 23:11:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+line = '123.456'
+digit = int(float(line) * 10) % 10
+print(digit)
+
