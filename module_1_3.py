f0418adff4229e30cc0ba2099eca54e3ed6caf0b
korapuzz <33680669+korapuzz@users.noreply.github.com>
2024-11-13 11:02:40 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -4,8 +4,8 @@ print("Name:", name)
 age = 54
 print("Age:", age)
 
-new_age = age + 1
-print("New age:", new_age)
+age += 1
+print("New age:", age)
 
 is_student = True
 print("Is Student:", is_student)
